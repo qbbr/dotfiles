e281d9633a9e5086458a01663537bd767cb431d6
Sokolov Innokenty <im@qbbr.io>
2020-03-10 14:27:57 +0800

    added ff user.js

---
+++ .mozilla/firefox/user.js
@@ -0,0 +1,89 @@
+// put to ff profile dir (about:support / about:profiles)
+
+//user_pref("general.buildID.override", "20100101");
+//user_pref("browser.startup.homepage_override.buildID", "20100101");
+//user_pref("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36");
+
+user_pref("dom.serviceWorkers.enabled", false);
+user_pref("dom.webnotifications.enabled", false);
+user_pref("dom.enable_performance", false);
+user_pref("dom.enable_user_timing", false);
+user_pref("dom.performance.enable_user_timing_logging", false);
+user_pref("dom.enable_resource_timing", false);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.telephony.enabled", false);
+user_pref("dom.gamepad.enabled", false);
+user_pref("dom.vr.enabled", false);
+user_pref("dom.vibrator.enabled", false);
+user_pref("dom.archivereader.enabled", false);
+user_pref("dom.flyweb.enabled", false);
+user_pref("dom.mozTCPSocket.enabled", false);
+user_pref("dom.netinfo.enabled", false);
+user_pref("dom.network.enabled", false);
+
+user_pref("app.update.auto", false);
+user_pref("browser.search.update", false);
+user_pref("loop.enabled", false);
+user_pref("loop.logDomains", false);
+user_pref("pdfjs.disabled", true);
+user_pref("browser.pocket.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.uitour.enabled", false);
+user_pref("camera.control.face_detection.enabled", false);
+user_pref("webgl.disabled", true);
+user_pref("webgl.disable-extension", true);
+user_pref("webgl.min_capability_mode", true);
+user_pref("network.proxy.socks_remote_dns", true);
+user_pref("network.manage-offline-status", false);
+user_pref("network.jar.open-unsafe-types", false);
+user_pref("beacon.enabled", false);
+user_pref("browser.send_pings", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.navigator.enabled", false);
+user_pref("media.navigator.video.enabled", false);
+user_pref("media.getusermedia.screensharing.enabled", false);
+user_pref("media.getusermedia.audiocapture.enabled", false);
+user_pref("media.webspeech.recognition.enable", false);
+user_pref("media.webspeech.synth.enabled", false);
+user_pref("media.video_stats.enabled", false);
+user_pref("device.sensors.enabled", false);
+user_pref("javascript.options.wasm", false);
+user_pref("javascript.options.asmjs", false);
+
+user_pref("plugins.click_to_play", true);
+
+user_pref("geo.enabled", false);
+user_pref("geo.wifi.logging.enabled", false);
+
+user_pref("security.mixed_content.block_active_content", true);
+user_pref("security.mixed_content.block_display_content", true);
+
+user_pref("extensions.getAddons.cache.enabled", false);
+user_pref("extensions.update.enabled", true);
+user_pref("extensions.systemAddon.update.enabled", false);
+user_pref("plugin.state.flash", 0);
+user_pref("plugin.state.java", 0);
+user_pref("plugin.state.libgnome-shell-browser-plugin", 0);
+user_pref("shumway.disabled", true);
+
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("experiments.supported", false);
+user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
+user_pref("network.allow-experiments", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.healthreport.service.enabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled",, false);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.selfsupport.url", "");
+
+user_pref("network.prefetch-next", false);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("network.predictor.enabled", false);
+user_pref("browser.casting.enabled", false);
